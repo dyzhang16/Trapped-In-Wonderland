a5ec8f35fc58a5021e6258b86e70a6fa258b6528
Daniel Zhang <dyzhang@ucsc.edu>
2020-05-08 07:44:30 -0700

    Initial push

---
+++ src/main.js
@@ -0,0 +1,8 @@
+let config = {                                                      //set configuration for phaser game
+    type: Phaser.CANVAS,                                            //game window is 640 by 480
+    width: 640, 
+    height: 480,
+    scene:[/*scenes */],
+}
+
+let game = new Phaser.Game(config);
